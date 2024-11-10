47e17212f21f2a2972e5c8142199ac6975634120
SuffleF <furkansemih07@gmail.com>
2024-11-11 00:45:16 +0300

    Project setup completed. Controllers, database connection, Routes, Models added in backend. Pages, components and some packages added in frontend

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schemq({
+    senderId: {
+        type: String,
+        required: true,
+    }, // Clerk user ID
+    receiverId: {
+        type: String,
+        required: true,
+    }, // Clerk user ID
+    content: {
+        type: String,
+        required: true,
+    },
+}, {timestamps: true}); // CreatedAt and UpdatedAt
+
+export const Message = mongoose.model('Message', messageSchema);
